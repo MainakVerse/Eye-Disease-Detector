144b5f2872bc5d3adab026aa524919807dcbe918
MAINAK CHAUDHURI <mainakchaudhuri671@gmail.com>
2025-03-01 19:22:16 +0530

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -47,7 +47,7 @@ def apply_tailwind_css():
         .card {
             padding: 0.5rem;
             border-radius: 0.75rem;
-            background-color: yellow;
+            background-color: white;
             margin: 0.5rem;
             box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
             margin-bottom: 0.5rem;
